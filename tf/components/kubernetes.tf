2c0b044c04cbb0120c17bec2fd0928b584545a7b
Chaiwat Klomchitcharoen <chaiwat.k@appman.co.th>
2020-08-14 18:59:04 +0700

    add docker-registry secret

--- tf/components/kubernetes.tf
+++ tf/components/kubernetes.tf
@@ -1,3 +1,19 @@
 provider "kubernetes" {
   version = "~> 1.12"
 }
+
+resource "kubernetes_namespace" "default_namespace" {
+  metadata {
+    name = var.namespaces
+  }
+
+  depends_on = [
+    helm_release.argocd_release,
+  ]
+}
+
+resource "null_resource" "secret" {
+  provisioner "local-exec" {     
+     command = "kubectl create secret docker-registry regcred --docker-server=https://index.docker.io/v1/ --docker-username=${var.dockerhub_user} --docker-password=${var.dockerhub_password} --docker-email=${var.email} -n ${kubernetes_namespace.default_namespace.metadata.0.name}"
+  }
+}
---
+++ tf/components/variables.tf
@@ -0,0 +1,16 @@
+variable "dockerhub_user" {
+  description = "Docker user"
+}
+
+variable "dockerhub_password" {
+  description = "Docker password"
+}
+
+variable "email" {
+  default = "contact@appman.co.th"
+  description = "Docker email"
+}
+
+variable "namespaces" {
+  description = "base namespaces"
+}
