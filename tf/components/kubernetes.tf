96c72f3345117896ca2a08914593ab7c719d116a
DESKTOP-JFELB92\AppManL-062 <brecht.p@appman.co.th>
2020-08-17 10:37:16 +0700

    WIP: create project on argocd

--- tf/components/kubernetes.tf
+++ tf/components/kubernetes.tf
@@ -13,7 +13,17 @@ resource "kubernetes_namespace" "default_namespace" {
 }
 
 resource "null_resource" "secret" {
+  #This step will FAIL unless port-forwarding is on, WIP
+  provisioner "local-exec" {
+    command = "argocd login localhost:8080 --username admin --password ${random_password.argopass.result} --insecure | argocd repo add https://github.com/${var.git_server}/${var.git_repository} --username ${var.git_username} --password ${var.git_password} --name ${var.git_repository} "
+  }
+  provisioner "local-exec" {
+    command = "argocd login localhost:8080 --username admin --password ${random_password.argopass.result} --insecure | argocd app create ${var.application_name} --repo https://github.com/${var.git_server}/${var.git_repository} --path ${var.yaml_config_directory} --dest-server https://kubernetes.default.svc --dest-namespace ${kubernetes_namespace.default_namespace.metadata.0.name}"
+  }
   provisioner "local-exec" {     
      command = "kubectl create secret docker-registry regcred --docker-server=https://index.docker.io/v1/ --docker-username=${var.dockerhub_user} --docker-password=${var.dockerhub_password} --docker-email=${var.email} -n ${kubernetes_namespace.default_namespace.metadata.0.name}"
   }
+  depends_on = [
+    kubernetes_namespace.project_namespace,
+  ]
 }
--- tf/components/variables.tf
+++ tf/components/variables.tf
@@ -14,3 +14,36 @@ variable "email" {
 variable "namespaces" {
   description = "base namespaces"
 }
+
+variable "git_server" {
+  type=string
+  default = var.git_username
+  description = "Git account the repository is located in"
+}
+
+variable "git_repository" {
+  type=string
+  description = "Git repository name (only the repo name)"
+}
+
+variable "git_username" {
+  type=string
+  description = "Git username to authenticate on the repository"
+}
+
+variable "git_password" {
+  type=string
+  description = "Git password to authenticate on the repository"
+}
+
+variable "application_name" {
+  description = "Name of the application in ArgoCD deployment"
+  default = "application"
+  type = string
+}
+
+variable "yaml_config_directory" {
+  description = "Directory containing the kubernetes or helm files"
+  default = "chart"
+  type = string
+}
